dd6bf977bf32ebc70628077f21dc0534f1b289f9
Isaac Pure <isaacpure@gmail.com>
2022-08-30 12:20:03 -0500

    commiting same as dev

--- src/App.js
+++ src/App.js
@@ -119,9 +119,7 @@ function App() {
     <div className="main">
       <h1>The App.Post</h1>
       <button onClick={newArticlePage}>Add New Article</button>
-
       <button onClick={showArticlesPage}>Show Article</button>
-
     { newArticleForm ?
       <section className='createForm'>
         <form className="newForm" onSubmit={handleNewArticleFormSubmit}>
