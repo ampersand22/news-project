50016c655343f0ed4f214ae5ce38d4d3f2beb2d5
Isaac Pure <isaacpure@gmail.com>
2022-08-30 10:19:22 -0500

    clearing any chagnes out

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ function App() {
     <div className="main">
       <h1>The App.Post</h1>
       <button onClick={newArticlePage}>Add New Article</button>
-      <button onClick={showArticlesPage}>Add New Article</button>
+      <button onClick={showArticlesPage}>Show Article</button>
     { newArticleForm ?
       <section className='createForm'>
         <form className="newForm" onSubmit={handleNewArticleFormSubmit}>
