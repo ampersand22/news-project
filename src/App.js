7a7c42fd843dbf51e618b3fc8878bbf1bc8558a2
Isaac Pure <isaacpure@gmail.com>
2022-09-01 11:45:44 -0500

    added logo to navbar

--- src/App.js
+++ src/App.js
@@ -116,7 +116,9 @@ function App() {
     <div className="container-fluid">
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
         <div className="container-fluid">
-          <a className="navbar-brand" onClick={showArticlesPage} href="#">The App.Post</a>
+          <a className="navbar-brand" onClick={showArticlesPage} href="#">
+            <img src="/app_post_logo.png" alt="" width="auto" height="auto"/>
+          </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
           </button>
